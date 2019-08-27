2dde4972ae242cf75f3e0e281c15b1950eec4061
lolauriss <lolauriss@gmail.com>
2019-08-27 16:21:31 +0000

    funcionando

--- hello-rigo/src/js/component/card.js
+++ hello-rigo/src/js/component/card.js
@@ -1,64 +1,75 @@
 import React, { Fragment } from "react";
 
 export function Card(props) {
+	let style = { width: "18rem" };
 	return (
 		<Fragment>
-        let style = {
-            "width: 18rem";
-        }
-			<div className="card" style="width: 18rem">
-				<img src="..." className="card-img-top" alt="..." />
-				<div className="card-body">
-					<h5 className="card-title">Card title</h5>
-					<p className="card-text">
-						Some quick example text to build on the card title and
-						make up the bulk of the card's content.
-					</p>
-					<a href="#" className="btn btn-primary">
-						Find Out More!
-					</a>
-				</div>
-			</div>
-
-			<div className="card" style="width: 18rem">
-				<img src="..." className="card-img-top" alt="..." />
-				<div className="card-body">
-					<h5 className="card-title">Card title</h5>
-					<p className="card-text">
-						Some quick example text to build on the card title and
-						make up the bulk of the card's content.
-					</p>
-					<a href="#" className="btn btn-primary">
-						Find Out More!
-					</a>
-				</div>
-			</div>
-
-			<div className="card" style="width: 18rem">
-				<img src="..." className="card-img-top" alt="..." />
-				<div className="card-body">
-					<h5 className="card-title">Card title</h5>
-					<p className="card-text">
-						Some quick example text to build on the card title and
-						make up the bulk of the card's content.
-					</p>
-					<a href="#" className="btn btn-primary">
-						Find Out More!
-					</a>
-				</div>
-			</div>
-
-			<div className="card" style="width: 18rem">
-				<img src="..." className="card-img-top" alt="..." />
-				<div className="card-body">
-					<h5 className="card-title">Card title</h5>
-					<p className="card-text">
-						Some quick example text to build on the card title and
-						make up the bulk of the card's content.
-					</p>
-					<a href="#" className="btn btn-primary">
-						Find Out More!
-					</a>
+			<div className="container">
+				<div className="row">
+					<div className="col-md-3">
+						<div className="card" style={style}>
+							<img src="..." className="card-img-top" alt="..." />
+							<div className="card-body">
+								<h5 className="card-title">Card title</h5>
+								<p className="card-text">
+									Some quick example text to build on the card
+									title and make up the bulk of the card's
+									content.
+								</p>
+								<a href="#" className="btn btn-primary">
+									Find Out More!
+								</a>
+							</div>
+						</div>
+					</div>
+					<div className="col-md-3">
+						<div className="card" style={style}>
+							<img src="..." className="card-img-top" alt="..." />
+							<div className="card-body">
+								<h5 className="card-title">Card title</h5>
+								<p className="card-text">
+									Some quick example text to build on the card
+									title and make up the bulk of the card's
+									content.
+								</p>
+								<a href="#" className="btn btn-primary">
+									Find Out More!
+								</a>
+							</div>
+						</div>
+					</div>
+					<div className="col-md-3">
+						<div className="card" style={style}>
+							<img src="..." className="card-img-top" alt="..." />
+							<div className="card-body">
+								<h5 className="card-title">Card title</h5>
+								<p className="card-text">
+									Some quick example text to build on the card
+									title and make up the bulk of the card's
+									content.
+								</p>
+								<a href="#" className="btn btn-primary">
+									Find Out More!
+								</a>
+							</div>
+						</div>
+					</div>
+					<div className="col-md-3">
+						<div className="card" style={style}>
+							<img src="..." className="card-img-top" alt="..." />
+							<div className="card-body">
+								<h5 className="card-title">Card title</h5>
+								<p className="card-text">
+									Some quick example text to build on the card
+									title and make up the bulk of the card's
+									content.
+								</p>
+								<a href="#" className="btn btn-primary">
+									Find Out More!
+								</a>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</Fragment>
